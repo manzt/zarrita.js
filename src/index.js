44a52a5d34b53e0d68edfcd46033ad4a7ade1d20
Trevor Manz <trevor.j.manz@gmail.com>
2021-12-08 09:59:51 -0500

    rename to .ts

--- src/index.js
+++
@@ -1,9 +0,0 @@
-// @ts-check
-export * as v2 from './v2.js';
-export * as v3 from './v3.js';
-export { get, set } from './lib/ops.js';
-export { slice } from './lib/util.js';
-export { registry } from './lib/codec-registry.js';
-export { is, is_numeric } from './lib/helpers.js';
-export { BoolArray, ByteStringArray, UnicodeStringArray } from './lib/custom-arrays.js';
-export { ExplicitGroup, Group, ImplicitGroup, ZarrArray } from './lib/hierarchy.js';
---
+++ src/index.ts
@@ -0,0 +1,9 @@
+export * as v2 from './v2';
+export * as v3 from './v3';
+
+export { get, set } from './lib/ops';
+export { slice } from './lib/util';
+export { registry } from './lib/codec-registry';
+export { is, is_numeric } from './lib/helpers';
+export { BoolArray, ByteStringArray, UnicodeStringArray } from './lib/custom-arrays';
+export { ExplicitGroup, Group, ImplicitGroup, ZarrArray } from './lib/hierarchy';
