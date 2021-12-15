f7549993f79ce204be476f0903e15d93e3b53884
Trevor Manz <trevor.j.manz@gmail.com>
2021-12-15 12:47:07 -0500

    Add default codecs

--- src/lib/codec-registry.ts
+++ src/lib/codec-registry.ts
@@ -4,3 +4,21 @@ type Config = Record<string, any>;
 type CodecImporter = () => Promise<{ fromConfig: (config: Config) => Codec }>;
 
 export const registry: Map<string, CodecImporter> = new Map();
+
+registry.set(...default_import("blosc"));
+registry.set(...default_import("zstd"));
+registry.set(...default_import("gzip"));
+registry.set(...default_import("zlib"));
+registry.set(...default_import("lz4"));
+
+function default_import(id: "blosc" | "zlib" | "zstd" | "gzip" | "lz4", version = "0.2.2") {
+	let link: string;
+	if (typeof process === "undefined") {
+		// browser
+		link = `https://cdn.skypack.dev/numcodecs@${version}/${id}`;
+	} else {
+	    // node
+		link = `numcodecs/${id}`;
+	}
+	return [id, (() => import(/* @vite-ignore */ link).then((m) => m.default)) as CodecImporter] as const;
+}
