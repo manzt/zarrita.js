bcaf9c4ac814f0dc1ad691603cee5ef14ece317d
Trevor Manz <trevor.j.manz@gmail.com>
2021-12-15 13:29:56 -0500

    Add experimental storage options (#37)

--- src/storage/index.ts
+++ src/storage/index.ts
@@ -1,3 +1,5 @@
 export { default as FetchStore } from "./fetch";
 export { default as FileSystemStore } from "./fs";
 export { default as MemoryStore } from "./mem";
+export { default as ReferenceStore } from "./ref";
+export { default as ZipFileStore } from "./zip";
