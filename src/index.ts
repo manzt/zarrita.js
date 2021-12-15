d502d24dbd363857acd5af6a384c703d6d0dc3d3
Trevor Manz <trevor.j.manz@gmail.com>
2021-12-14 20:51:06 -0500

    change top-level exports

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,7 @@
 export * as v2 from "./v2";
 export * as v3 from "./v3";
-export { get, set } from "./ops";
+export * as ops from "./ops";
+export * as ndarray from "./ndarray";
 export { slice } from "./lib/util";
 export { registry } from "./lib/codec-registry";
-export * as ndarray from "./ndarray";
 export * from "./types";
