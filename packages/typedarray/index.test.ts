4590136c5d8070d8fc46d0c86ef3973d8b3ff0c8
Trevor Manz <trevor.j.manz@gmail.com>
2023-07-25 17:32:13 +0200

    migrate tests

--- packages/typedarray/index.test.ts
+++ packages/typedarray/index.test.ts
@@ -1,26 +1,26 @@
 import { test, assert } from "vitest";
 
-import { BoolArray, ByteStringArray, UnicodeStringArray } from "../index.ts";
+import { BoolArray, ByteStringArray, UnicodeStringArray } from "./index.ts";
 
 test("BoolArray", () => {
 	let arr = new BoolArray(5);
-	assert.equal(arr.length, 5);
-	assert.equal([false, false, false, false, false], Array.from(arr));
-	assert.equal(arr.get(1), false);
+	expect(arr.length).toBe(5);
+	expect([false, false, false, false, false]).toBe(Array.from(arr));
+	expect(arr.get(1)).toBe(false);
 	arr.set(1, true);
-	assert.equal(arr.get(1), true);
+	expect(arr.get(1)).toBe(true);
 	arr.set(3, true);
-	assert.equal([false, true, false, true, false], Array.from(arr));
+	expect([false, true, false, true, false]).toBe(Array.from(arr));
 
 	let bytes = new Uint8Array([1, 1, 0, 0]);
 	arr = new BoolArray(bytes.buffer);
-	assert.equal(arr.length, 4);
-	assert.equal(arr.get(0), true);
-	assert.equal(arr.get(3), false);
-	assert.equal([true, true, false, false], Array.from(arr));
+	expect(arr.length).toBe(4);
+	expect(arr.get(0)).toBe(true);
+	expect(arr.get(3)).toBe(false);
+	expect([true, true, false, false]).toBe(Array.from(arr));
 
 	arr.fill(true);
-	assert.equal([true, true, true, true], Array.from(arr));
+	expect([true, true, true, true]).toBe(Array.from(arr));
 });
 
 test("ByteStringArray", () => {
@@ -28,14 +28,11 @@ test("ByteStringArray", () => {
 		"Hello\x00world!\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00",
 	);
 	let arr = new ByteStringArray(data, 6);
-	assert.equal(arr.length, 5);
-	assert.equal(["Hello", "world!", "", "", ""], Array.from(arr));
+	expect(arr.length).toBe(5);
+	expect(["Hello", "world!", "", "", ""]).toBe(Array.from(arr));
 	arr.set(4, "test");
-	assert.equal(["Hello", "world!", "", "", "test"], Array.from(arr));
-	assert.equal(
-		new TextDecoder().decode(arr.buffer),
-		"Hello\x00world!\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00test\x00\x00",
-	);
+	expect(["Hello", "world!", "", "", "test"]).toBe(Array.from(arr));
+	expect(new TextDecoder().decode(arr.buffer)).toBe("Hello\x00world!\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00test\x00\x00");
 	assert.throws(() => arr.set(3, "too-long"));
 });
 
@@ -43,18 +40,18 @@ test("UnicodeStringArray", () => {
 	// deno-fmt-ignore
 	let data = new Int32Array([ 72, 101, 108, 108, 111, 0, 0, 0, 0, 0, 119, 111, 114, 108, 100, 33, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]);
 	let arr = new UnicodeStringArray(data.buffer, 10);
-	assert.equal(arr.length, 3);
-	assert.equal(["Hello", "world!", ""], Array.from(arr));
+	expect(arr.length).toBe(3);
+	expect(["Hello", "world!", ""]).toBe(Array.from(arr));
 	arr.set(2, "test");
-	assert.equal(["Hello", "world!", "test"], Array.from(arr));
+	expect(["Hello", "world!", "test"]).toBe(Array.from(arr));
 });
 
 test("UnicodeStringArray - special characters", () => {
 	// deno-fmt-ignore
 	let data = new Int32Array([161, 72, 111, 108, 97, 32, 109, 117, 110, 100, 111, 33, 0, 0, 0, 0, 0, 0, 0, 0, 72, 101, 106, 32, 86, 228, 114, 108, 100, 101, 110, 33, 0, 0, 0, 0, 0, 0, 0, 0, 88, 105, 110, 32, 99, 104, 224, 111, 32, 116, 104, 7871, 32, 103, 105, 7899, 105, 0, 0, 0]);
 	let arr = new UnicodeStringArray(data.buffer, 20);
-	assert.equal(arr.length, 3);
-	assert.equal(["¡Hola mundo!", "Hej Världen!", "Xin chào thế giới"], Array.from(arr));
+	expect(arr.length).toBe(3);
+	expect(["¡Hola mundo!", "Hej Världen!", "Xin chào thế giới"]).toBe(Array.from(arr));
 	arr.set(2, "test");
-	assert.equal(["¡Hola mundo!", "Hej Världen!", "test"], Array.from(arr));
+	expect(["¡Hola mundo!", "Hej Världen!", "test"]).toBe(Array.from(arr));
 });
