472c98f5d2aa3f30a7258f08fa06232214680e2a
Trevor Manz <trevor.j.manz@gmail.com>
2023-08-08 11:28:58 -0400

    test(util): clean-up utils tests

--- packages/core/__tests__/indexing.test.ts
+++ packages/core/__tests__/indexing.test.ts
@@ -1,4 +1,4 @@
-import { describe, expect, it } from "vitest";
+import { describe, expect, test } from "vitest";
 import type { Slice } from "../src/types.js";
 import {
 	BasicIndexer,
@@ -16,45 +16,54 @@ describe("normalize_selection", () => {
 			b.map((s) => s.toString()),
 		);
 	};
-	it("handles complete selection", () => {
+	test("handles complete selection", () => {
 		eq(
 			normalize_selection(null, [2, 3, 4]),
 			[slice(null), slice(null), slice(null)],
 		);
 	});
-	it("handles partial complete selection", () => {
+	test("handles partial complete selection", () => {
 		eq(
 			normalize_selection([slice(2), null, 3], [2, 3, 4]),
 			[slice(2), slice(null), 3],
 		);
 	});
-	it("throws when dimensions don't match", () => {
+	test("throws when dimensions don't match", () => {
 		expect(() => normalize_selection([slice(2), null, 3, 4], [2, 3, 4]))
 			.toThrowError();
 	});
 });
 
 describe("normalize_integer_selection", () => {
-	it.each([
+	test.each([
 		[2, 5, 2],
 		[-1, 5, 4],
 		[-2, 5, 3],
 		[-2.2, 5, 3],
 		[4.3, 5, 4],
-	])(`(dim_selection=%i, dim_length=%i) -> %i`, (dim_selection, dim_length, expected) => {
-		expect(normalize_integer_selection(dim_selection, dim_length)).toBe(expected);
-	});
-	it.each([
+	])(
+		`normalize_integer_selection(%i, %i) -> %i`,
+		(dim_selection, dim_length, expected) => {
+			expect(normalize_integer_selection(dim_selection, dim_length)).toBe(
+				expected,
+			);
+		},
+	);
+	test.each([
 		[5, 5],
 		[6, 5],
 		[-6, 5],
-	])(`(dim_selection=%i, dim_length=%i) -> throws`, (dim_selection, dim_length) => {
-		expect(() => normalize_integer_selection(dim_selection, dim_length)).toThrowError();
-	});
+	])(
+		`normalize_integer_selection(%i, %i) -> throws`,
+		(dim_selection, dim_length) => {
+			expect(() => normalize_integer_selection(dim_selection, dim_length))
+				.toThrowError();
+		},
+	);
 });
 
 describe("BasicIndexer", () => {
-	it("handles identical shape and chunk_shape", () => {
+	test("handles identical shape and chunk_shape", () => {
 		let indexer = new BasicIndexer({
 			selection: null,
 			shape: [3, 4, 5],
@@ -105,7 +114,7 @@ describe("BasicIndexer", () => {
 		`);
 	});
 
-	it("handles complete multi-dim chunking", () => {
+	test("handles complete multi-dim chunking", () => {
 		let indexer = new BasicIndexer({
 			selection: null,
 			shape: [3, 4, 5],
@@ -387,7 +396,7 @@ describe("BasicIndexer", () => {
 		`);
 	});
 
-	it("handles squeezed dimension", () => {
+	test("handles squeezed dimension", () => {
 		let indexer = new BasicIndexer({
 			selection: [null, 0],
 			shape: [3, 4],
