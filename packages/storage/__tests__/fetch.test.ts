3173ba0b06eed0ac36b5a31f1177f9cd20f15b37
Trevor Manz <trevor.j.manz@gmail.com>
2023-08-15 10:48:00 -0400

    cleanup tests

--- packages/storage/__tests__/fetch.test.ts
+++ packages/storage/__tests__/fetch.test.ts
@@ -2,165 +2,59 @@ import { afterEach, describe, expect, it, vi } from "vitest";
 
 import FetchStore from "../src/fetch.js";
 
+// `vitest --api` exposes the port 51204
+// ref: https://vitest.dev/config/#api
+let href = "http://localhost:51204/fixtures/v3/data.zarr";
+
 describe("FetchStore", () => {
 	afterEach(() => {
 		vi.restoreAllMocks();
 	});
 
 	it("reads a file from string url", async () => {
-		let store = new FetchStore("http://localhost:51204/fixtures/v3/data.zarr");
-		expect(await store.get("/zarr.json")).toMatchInlineSnapshot(`
-			Uint8Array [
-			  123,
-			  34,
-			  97,
-			  116,
-			  116,
-			  114,
-			  105,
-			  98,
-			  117,
-			  116,
-			  101,
-			  115,
-			  34,
-			  58,
-			  32,
-			  123,
-			  125,
-			  44,
-			  32,
-			  34,
-			  122,
-			  97,
-			  114,
-			  114,
-			  95,
-			  102,
-			  111,
-			  114,
-			  109,
-			  97,
-			  116,
-			  34,
-			  58,
-			  32,
-			  51,
-			  44,
-			  32,
-			  34,
-			  110,
-			  111,
-			  100,
-			  101,
-			  95,
-			  116,
-			  121,
-			  112,
-			  101,
-			  34,
-			  58,
-			  32,
-			  34,
-			  103,
-			  114,
-			  111,
-			  117,
-			  112,
-			  34,
-			  125,
-			]
+		let store = new FetchStore(href);
+		let bytes = await store.get("/zarr.json");
+		expect(bytes).toBeInstanceOf(Uint8Array);
+		expect(JSON.parse(new TextDecoder().decode(bytes))).toMatchInlineSnapshot(`
+			{
+			  "attributes": {},
+			  "node_type": "group",
+			  "zarr_format": 3,
+			}
 		`);
 	});
+
 	it("reads a file from URL", async () => {
-		let store = new FetchStore(
-			new URL("http://localhost:51204/fixtures/v3/data.zarr"),
-		);
-		expect(await store.get("/zarr.json")).toMatchInlineSnapshot(`
-			Uint8Array [
-			  123,
-			  34,
-			  97,
-			  116,
-			  116,
-			  114,
-			  105,
-			  98,
-			  117,
-			  116,
-			  101,
-			  115,
-			  34,
-			  58,
-			  32,
-			  123,
-			  125,
-			  44,
-			  32,
-			  34,
-			  122,
-			  97,
-			  114,
-			  114,
-			  95,
-			  102,
-			  111,
-			  114,
-			  109,
-			  97,
-			  116,
-			  34,
-			  58,
-			  32,
-			  51,
-			  44,
-			  32,
-			  34,
-			  110,
-			  111,
-			  100,
-			  101,
-			  95,
-			  116,
-			  121,
-			  112,
-			  101,
-			  34,
-			  58,
-			  32,
-			  34,
-			  103,
-			  114,
-			  111,
-			  117,
-			  112,
-			  34,
-			  125,
-			]
+		let store = new FetchStore(new URL(href));
+		let bytes = await store.get("/zarr.json");
+		expect(bytes).toBeInstanceOf(Uint8Array);
+		expect(JSON.parse(new TextDecoder().decode(bytes))).toMatchInlineSnapshot(`
+			{
+			  "attributes": {},
+			  "node_type": "group",
+			  "zarr_format": 3,
+			}
 		`);
 	});
+
 	it("returns undefined for missing file", async () => {
-		let store = new FetchStore("http://localhost:51204/fixtures/v3/data.zarr");
+		let store = new FetchStore(href);
 		expect(await store.get("/missing.json")).toBeUndefined();
 	});
+
 	it("it forwards request options to fetch", async () => {
-		let store = new FetchStore("http://localhost:51204/fixtures/v3/data.zarr");
+		let headers = { "x-test": "test" };
+		let store = new FetchStore(href);
 		let spy = vi.spyOn(globalThis, "fetch");
-		await store.get("/zarr.json", { headers: { "x-test": "test" } });
-		expect(spy).toHaveBeenCalledWith(
-			"http://localhost:51204/fixtures/v3/data.zarr/zarr.json",
-			{ headers: { "x-test": "test" } },
-		);
+		await store.get("/zarr.json", { headers });
+		expect(spy).toHaveBeenCalledWith(href + "/zarr.json", { headers });
 	});
+
 	it("it forwards request options to fetch when configured globally", async () => {
-		let store = new FetchStore("http://localhost:51204/fixtures/v3/data.zarr", {
-			headers: { "x-test": "foo" },
-		});
+		let headers = { "x-test": "test" };
+		let store = new FetchStore(href, { headers });
 		let spy = vi.spyOn(globalThis, "fetch");
 		await store.get("/zarr.json");
-		expect(spy).toHaveBeenCalledWith(
-			"http://localhost:51204/fixtures/v3/data.zarr/zarr.json",
-			{ headers: { "x-test": "foo" } },
-		);
+		expect(spy).toHaveBeenCalledWith(href + "/zarr.json", { headers });
 	});
 });
