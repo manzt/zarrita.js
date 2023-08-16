bb0e7db2bb34602415436768385b015c8d18321a
Trevor Manz <trevor.j.manz@gmail.com>
2023-08-15 23:37:14 -0400

    migrate open to JSDoc

--- packages/core/src/open.js
+++ packages/core/src/open.js
@@ -1,5 +1,3 @@
-import type { Async, Readable } from "@zarrita/storage";
-import type { ArrayMetadata, DataType, GroupMetadata } from "./metadata.js";
 import { Array, Group, Location } from "./hierarchy.js";
 import { NodeNotFoundError } from "./errors.js";
 import {
@@ -8,29 +6,52 @@ import {
 	v2_to_v3_group_metadata,
 } from "./util.js";
 
-function open_v2<Store extends Readable | Async<Readable>>(
-	location: Location<Store> | Store,
-	options: { kind: "group" },
-): Promise<Group<Store>>;
+/** @typedef {import("./metadata.js").DataType} DataType */
+/** @typedef {import("@zarrita/storage").Readable} Readable */
+/**
+ * @template {Record<string, any>} T
+ * @typedef {import("@zarrita/storage").Async<T>} Async
+ */
 
-function open_v2<Store extends Readable | Async<Readable>>(
-	location: Location<Store> | Store,
-	options: { kind: "array" },
-): Promise<Array<DataType, Store>>;
-
-function open_v2<Store extends Readable | Async<Readable>>(
-	location: Location<Store> | Store,
-	options: { kind: "auto" },
-): Promise<Array<DataType, Store> | Group<Store>>;
-
-function open_v2<Store extends Readable | Async<Readable>>(
-	location: Location<Store> | Store,
-): Promise<Array<DataType, Store> | Group<Store>>;
-
-async function open_v2<Store extends Readable | Async<Readable>>(
-	location: Location<Store> | Store,
-	options: { kind: "auto" | "array" | "group" } = { kind: "auto" },
-) {
+/**
+ * Open a v2 array.
+ *
+ * @template {Readable | Async<Readable>} Store
+ * @overload
+ * @param {Location<Store> | Store} location
+ * @param {{ kind: "array" }} options
+ * @returns {Promise<Array<DataType, Store>>}
+ * @throws {NodeNotFoundError}
+ */
+/**
+ * Open a v2 group.
+ *
+ * @template {Readable | Async<Readable>} Store
+ * @overload
+ * @param {Location<Store> | Store} location
+ * @param {{ kind: "group" }} options
+ * @returns {Promise<Group<Store>>}
+ * @throws {NodeNotFoundError}
+ */
+/**
+ * Open a v2 array or group.
+ *
+ * @template {Readable | Async<Readable>} Store
+ * @overload
+ * @param {Location<Store> | Store} location
+ * @returns {Promise<Array<DataType, Store> | Group<Store>>}
+ * @throws {NodeNotFoundError}
+ */
+/**
+ * Open a v2 array or group.
+ *
+ * @template {Readable | Async<Readable>} Store
+ * @param {Location<Store> | Store} location
+ * @param {{ kind?: "array" | "group" }} [options]
+ * @returns {Promise<Array<DataType, Store> | Group<Store>>}
+ * @throws {NodeNotFoundError}
+ */
+async function open_v2(location, options = {}) {
 	let loc = "store" in location ? location : new Location(location);
 	if (options.kind === "array") return open_array_v2(loc);
 	if (options.kind === "group") return open_group_v2(loc);
@@ -40,9 +61,13 @@ async function open_v2<Store extends Readable | Async<Readable>>(
 	});
 }
 
-async function open_array_v2<Store extends Readable | Async<Readable>>(
-	location: Location<Store>,
-) {
+/**
+ * @template {Readable | Async<Readable>} Store
+ * @param {Location<Store>} location
+ * @returns {Promise<Array<DataType, Store>>}
+ * @throws {NodeNotFoundError}
+ */
+async function open_array_v2(location) {
 	let { path } = location.resolve(".zarray");
 	let meta = await location.store.get(path);
 	if (!meta) {
@@ -55,9 +80,13 @@ async function open_array_v2<Store extends Readable | Async<Readable>>(
 	);
 }
 
-async function open_group_v2<Store extends Readable | Async<Readable>>(
-	location: Location<Store>,
-) {
+/**
+ * @template {Readable | Async<Readable>} Store
+ * @param {Location<Store>} location
+ * @returns {Promise<Group<Store>>}
+ * @throws {NodeNotFoundError}
+ */
+async function open_group_v2(location) {
 	let { path } = location.resolve(".zgroup");
 	let meta = await location.store.get(path);
 	if (!meta) {
@@ -70,80 +99,106 @@ async function open_group_v2<Store extends Readable | Async<Readable>>(
 	);
 }
 
-async function _open_v3<Store extends Readable | Async<Readable>>(
-	location: Location<Store>,
-) {
+/**
+ * @template {Readable | Async<Readable>} Store
+ * @param {Location<Store>} location
+ * @returns {Promise<Array<DataType, Store> | Group<Store>>}
+ */
+async function _open_v3(location) {
 	let { store, path } = location.resolve("zarr.json");
 	let meta = await location.store.get(path);
 	if (!meta) {
 		throw new NodeNotFoundError(path);
 	}
-	let meta_doc: ArrayMetadata<DataType> | GroupMetadata = json_decode_object(
-		meta,
-	);
+	/** @type {import("./metadata.js").ArrayMetadata<DataType> | import("./metadata.js").GroupMetadata} */
+	let meta_doc = json_decode_object(meta);
 	return meta_doc.node_type === "array"
 		? new Array(store, location.path, meta_doc)
 		: new Group(store, location.path, meta_doc);
 }
 
-function open_v3<Store extends Readable | Async<Readable>>(
-	location: Location<Store> | Store,
-	options: { kind: "group" },
-): Promise<Group<Store>>;
-
-function open_v3<Store extends Readable | Async<Readable>>(
-	location: Location<Store> | Store,
-	options: { kind: "array" },
-): Promise<Array<DataType, Store>>;
-
-function open_v3<Store extends Readable | Async<Readable>>(
-	location: Location<Store> | Store,
-	options: { kind: "auto" },
-): Promise<Array<DataType, Store> | Group<Store>>;
-
-function open_v3<Store extends Readable | Async<Readable>>(
-	location: Location<Store> | Store,
-): Promise<Array<DataType, Store> | Group<Store>>;
-
-async function open_v3<Store extends Readable | Async<Readable>>(
-	location: Location<Store>,
-	options: { kind: "auto" | "array" | "group" } = { kind: "auto" },
-): Promise<Array<DataType, Store> | Group<Store>> {
+/**
+ * Open a v3 group.
+ * @template {Readable | Async<Readable>} Store
+ * @overload
+ * @param {Location<Store> | Store} location
+ * @param {{ kind: "group" }} options
+ * @returns {Promise<Group<Store>>}
+ * @throws {NodeNotFoundError}
+ */
+/**
+ * Open a v3 array.
+ * @template {Readable | Async<Readable>} Store
+ * @overload
+ * @param {Location<Store> | Store} location
+ * @param {{ kind: "array" }} options
+ * @returns {Promise<Array<DataType, Store>>}
+ * @throws {NodeNotFoundError}
+ */
+/**
+ * Open a v3 array or group.
+ * @template {Readable | Async<Readable>} Store
+ * @overload
+ * @param {Location<Store> | Store} location
+ * @returns {Promise<Array<DataType, Store> | Group<Store>>}
+ * @throws {NodeNotFoundError}
+ */
+/**
+ * Open a v3 array or group.
+ * @template {Readable | Async<Readable>} Store
+ * @param {Location<Store> | Store} location
+ * @param {{ kind?: "array" | "group" }} [options]
+ * @returns {Promise<Array<DataType, Store> | Group<Store>>}
+ * @throws {NodeNotFoundError}
+ */
+async function open_v3(location, options = {}) {
 	let loc = "store" in location ? location : new Location(location);
 	let node = await _open_v3(loc);
-	if (options.kind === "auto") return node;
+	if (options.kind === undefined) return node;
 	if (options.kind === "array" && node instanceof Array) return node;
 	if (options.kind === "group" && node instanceof Group) return node;
 	let kind = node instanceof Array ? "array" : "group";
 	throw new Error(`Expected node of kind ${options.kind}, found ${kind}.`);
 }
 
-export function open<Store extends Readable | Async<Readable>>(
-	location: Location<Store> | Store,
-	options: { kind: "group" },
-): Promise<Group<Store>>;
-
-export function open<Store extends Readable | Async<Readable>>(
-	location: Location<Store> | Store,
-	options: { kind: "array" },
-): Promise<Array<DataType, Store>>;
-
-export function open<Store extends Readable | Async<Readable>>(
-	location: Location<Store> | Store,
-	options: { kind: "auto" },
-): Promise<Array<DataType, Store> | Group<Store>>;
-
-export function open<Store extends Readable | Async<Readable>>(
-	location: Location<Store> | Store,
-): Promise<Array<DataType, Store> | Group<Store>>;
-
-export async function open<Store extends Readable | Async<Readable>>(
-	location: Location<Store> | Store,
-	options: { kind: "auto" | "array" | "group" } = { kind: "auto" },
-): Promise<Array<DataType, Store> | Group<Store>> {
-	return open_v3(location, options as any).catch((err) => {
+/**
+ * Open a Zarr group.
+ * @template {Readable | Async<Readable>} Store
+ * @overload
+ * @param {Location<Store> | Store} location
+ * @param {{ kind: "group" }} options
+ * @returns {Promise<Group<Store>>}
+ * @throws {NodeNotFoundError}
+ */
+/**
+ * Open a Zarr array.
+ * @template {Readable | Async<Readable>} Store
+ * @overload
+ * @param {Location<Store> | Store} location
+ * @param {{ kind: "array" }} options
+ * @returns {Promise<Array<DataType, Store>>}
+ * @throws {NodeNotFoundError}
+ */
+/**
+ * Open a Zarr array or group.
+ * @template {Readable | Async<Readable>} Store
+ * @overload
+ * @param {Location<Store> | Store} location
+ * @returns {Promise<Array<DataType, Store> | Group<Store>>}
+ * @throws {NodeNotFoundError}
+ */
+/**
+ * Open a Zarr array or group.
+ * @template {Readable | Async<Readable>} Store
+ * @param {Location<Store> | Store} location
+ * @param {{ kind?: "array" | "group" }} [options]
+ * @returns {Promise<Array<DataType, Store> | Group<Store>>}
+ * @throws {NodeNotFoundError}
+ */
+export async function open(location, options = {}) {
+	return open_v3(location, /** @type {any} */ (options)).catch((err) => {
 		if (err instanceof NodeNotFoundError) {
-			return open_v2(location, options as any);
+			return open_v2(location, /** @type {any} */ (options));
 		}
 		throw err;
 	});
