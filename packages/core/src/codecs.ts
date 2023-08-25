877edd6c64cc8229a87c84dd9744ac8f3a0e58ff
Trevor Manz <trevor.j.manz@gmail.com>
2023-08-25 08:40:01 -0400

    fix: codec support

--- packages/core/src/codecs.ts
+++ packages/core/src/codecs.ts
@@ -2,7 +2,7 @@ import type { Codec } from "numcodecs";
 import type { Chunk, CodecMetadata, DataType } from "./metadata.js";
 
 import { TransposeCodec } from "./codecs/transpose.js";
-import { EndianCodec } from "./codecs/endian.js";
+import { BytesCodec } from "./codecs/bytes.js";
 import { Crc32cCodec } from "./codecs/crc32c.js";
 import { VLenUTF8 } from "./codecs/vlen-utf8.js";
 
@@ -28,7 +28,7 @@ function create_default_registry(): Map<
 		.set("zlib", () => import("numcodecs/zlib").then((m) => m.default))
 		.set("zstd", () => import("numcodecs/zstd").then((m) => m.default))
 		.set("transpose", () => TransposeCodec)
-		.set("endian", () => EndianCodec)
+		.set("bytes", () => BytesCodec)
 		.set("crc32c", () => Crc32cCodec)
 		.set("vlen-utf8", () => VLenUTF8);
 }
@@ -89,9 +89,7 @@ async function load_codecs<D extends DataType>(chunk_meta: ChunkMetadata<D>) {
 		return { Codec, meta };
 	});
 	let array_to_array: ArrayToArrayCodec<D>[] = [];
-	let array_to_bytes: ArrayToBytesCodec<D> = (EndianCodec.fromConfig as any)({
-		endian: "little",
-	}, chunk_meta);
+	let array_to_bytes: ArrayToBytesCodec<D> | undefined;
 	let bytes_to_bytes: BytesToBytesCodec[] = [];
 	for await (let { Codec, meta } of promises) {
 		let codec = Codec.fromConfig(meta.configuration, chunk_meta);
@@ -107,6 +105,18 @@ async function load_codecs<D extends DataType>(chunk_meta: ChunkMetadata<D>) {
 		}
 	}
 	if (!array_to_bytes) {
+		if (!is_typed_array_like_meta(chunk_meta)) {
+			throw new Error(
+				`Cannot encode ${chunk_meta.data_type} to bytes without a codec`,
+			);
+		}
+		array_to_bytes = BytesCodec.fromConfig({ endian: "little" }, chunk_meta);
 	}
 	return { array_to_array, array_to_bytes, bytes_to_bytes };
+}
+
+function is_typed_array_like_meta<D extends DataType>(
+	meta: ChunkMetadata<D>,
+): meta is ChunkMetadata<Exclude<D, "v2:object">> {
+	return meta.data_type !== "v2:object";
 }
