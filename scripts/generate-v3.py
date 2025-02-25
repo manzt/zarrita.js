e71560888ad0fc00c161a12c26f6392ba2627ad1
Trevor Manz <trevor.j.manz@gmail.com>
2025-02-25 14:53:57 -0500

    chore: Migrate fixtures to zarr-python v3 (#251)
    * chore: Migrate fixtures to zarr-python v3
    
    * Format existing fixtures
    
    * update snapshots
    
    * Upate fixtures

--- scripts/generate-v3.py
+++ scripts/generate-v3.py
@@ -1,154 +1,142 @@
 # /// script
 # requires-python = ">=3.13"
-# dependencies = ["zarrita==0.2.7"]
+# dependencies = ["zarr"]
+#
+# [tool.uv]
+# exclude-newer = "2025-02-24T10:57:26.55032-05:00"
 # ///
 
 import shutil
 import pathlib
 import json
 
-import zarrita
+import zarr
+import zarr.codecs
+import zarr.storage
 import numpy as np
 
 SELF_DIR = pathlib.Path(__file__).parent
 ROOT = SELF_DIR / ".." / "fixtures" / "v3" / "data.zarr"
 
+np.random.seed(42)
+
 shutil.rmtree(ROOT, ignore_errors=True)
 
-store = zarrita.LocalStore(ROOT)
-zarrita.Group.create(store)
+store = zarr.storage.LocalStore(ROOT)
+zarr.create_group(store)
 
 # 1d.contiguous.gzip.i2
-a = zarrita.Array.create(
-    store / "1d.contiguous.gzip.i2",
+a = zarr.create_array(
+    store,
+    name="1d.contiguous.gzip.i2",
     dtype="int16",
     shape=(4,),
-    chunk_shape=(4,),
-    codecs=[
-        zarrita.codecs.bytes_codec(),
-        zarrita.codecs.gzip_codec(),
-    ],
+    chunks=(4,),
+    compressors=[zarr.codecs.GzipCodec()],
 )
 a[:] = [1, 2, 3, 4]
 
 # 1d.contiguous.blosc.i2
-a = zarrita.Array.create(
-    store / "1d.contiguous.blosc.i2",
+a = zarr.create_array(
+    store,
+    name="1d.contiguous.blosc.i2",
     dtype="int16",
     shape=(4,),
-    chunk_shape=(4,),
-    codecs=[
-        zarrita.codecs.bytes_codec(),
-        zarrita.codecs.blosc_codec(typesize=4),
-    ],
+    chunks=(4,),
+    compressors=[zarr.codecs.BloscCodec(typesize=4, shuffle="noshuffle")],
 )
 a[:] = [1, 2, 3, 4]
 
 # 1d.contiguous.raw.i2
-a = zarrita.Array.create(
-    store / "1d.contiguous.raw.i2",
+a = zarr.create_array(
+    store,
+    name="1d.contiguous.raw.i2",
     dtype="int16",
     shape=(4,),
-    chunk_shape=(4,),
-    codecs=[
-        zarrita.codecs.bytes_codec(),
-    ],
+    chunks=(4,),
+    compressors=None,
 )
 a[:] = [1, 2, 3, 4]
 
 
 # 1d.contiguous.i4
-a = zarrita.Array.create(
-    store / "1d.contiguous.i4",
+a = zarr.create_array(
+    store,
+    name="1d.contiguous.i4",
     dtype="int32",
-    chunk_shape=(4,),
+    chunks=(4,),
     shape=(4,),
-    codecs=[
-        zarrita.codecs.bytes_codec(),
-        zarrita.codecs.blosc_codec(typesize=4),
-    ],
+    compressors=[zarr.codecs.BloscCodec(typesize=4, shuffle="noshuffle")],
 )
 a[:] = [1, 2, 3, 4]
 
 
 # 1d.contiguous.u1
-a = zarrita.Array.create(
-    store / "1d.contiguous.u1",
+a = zarr.create_array(
+    store,
+    name="1d.contiguous.u1",
     dtype="uint8",
-    chunk_shape=(4,),
+    chunks=(4,),
     shape=(4,),
-    codecs=[
-        zarrita.codecs.bytes_codec(),
-        zarrita.codecs.blosc_codec(typesize=4),
-    ],
+    compressors=[zarr.codecs.BloscCodec(typesize=4, shuffle="noshuffle")],
 )
 a[:] = [255, 0, 255, 0]
 
 
 # 1d.contiguous.<f4
-a = zarrita.Array.create(
-    store / "1d.contiguous.f4.le",
+a = zarr.create_array(
+    store,
+    name="1d.contiguous.f4.le",
     dtype="float32",
-    chunk_shape=(4,),
+    chunks=(4,),
     shape=(4,),
-    codecs=[
-        zarrita.codecs.bytes_codec(),
-        zarrita.codecs.blosc_codec(typesize=4),
-    ],
+    compressors=[zarr.codecs.BloscCodec(typesize=4, shuffle="noshuffle")],
 )
 a[:] = [-1000.5, 0, 1000.5, 0]
 
 # 1d.contiguous.>f4
-a = zarrita.Array.create(
-    store / "1d.contiguous.f4.be",
+a = zarr.create_array(
+    store,
+    name="1d.contiguous.f4.be",
     dtype="float32",
-    chunk_shape=(4,),
+    chunks=(4,),
     shape=(4,),
-    codecs=[
-        zarrita.codecs.bytes_codec(endian="big"),
-        zarrita.codecs.blosc_codec(typesize=4),
-    ],
+    compressors=[zarr.codecs.BloscCodec(typesize=4, shuffle="noshuffle")],
 )
 a[:] = [-1000.5, 0, 1000.5, 0]
 
 # 1d.contiguous.f8
-a = zarrita.Array.create(
-    store / "1d.contiguous.f8",
+a = zarr.create_array(
+    store,
+    name="1d.contiguous.f8",
     dtype="float64",
-    chunk_shape=(4,),
+    chunks=(4,),
     shape=(4,),
-    codecs=[
-        zarrita.codecs.bytes_codec(),
-        zarrita.codecs.blosc_codec(typesize=4),
-    ],
+    compressors=[zarr.codecs.BloscCodec(typesize=4, shuffle="noshuffle")],
 )
 a[:] = [1.5, 2.5, 3.5, 4.5]
 
 
 # 1d.contiguous.b1
-a = zarrita.Array.create(
-    store / "1d.contiguous.b1",
+a = zarr.create_array(
+    store,
+    name="1d.contiguous.b1",
     dtype="bool",
-    chunk_shape=(4,),
+    chunks=(4,),
     shape=(4,),
-    codecs=[
-        zarrita.codecs.bytes_codec(),
-        zarrita.codecs.blosc_codec(typesize=4),
-    ],
+    compressors=[zarr.codecs.BloscCodec(typesize=4, shuffle="noshuffle")],
 )
 a[:] = [True, False, True, False]
 
 
 # 1d.chunked.i2
-a = zarrita.Array.create(
-    store / "1d.chunked.i2",
+a = zarr.create_array(
+    store,
+    name="1d.chunked.i2",
     dtype="int16",
-    chunk_shape=(2,),
+    chunks=(2,),
     shape=(4,),
-    codecs=[
-        zarrita.codecs.bytes_codec(),
-        zarrita.codecs.blosc_codec(typesize=4),
-    ],
+    compressors=[zarr.codecs.BloscCodec(typesize=4, shuffle="noshuffle")],
 )
 a[:] = [1, 2, 3, 4]
 
@@ -162,107 +150,93 @@ with open(ROOT / "1d.chunked.i2/zarr.json", "r") as f:
         json.dump(meta, f)
 
 # 1d.chunked.ragged.i2
-a = zarrita.Array.create(
-    store / "1d.chunked.ragged.i2",
+a = zarr.create_array(
+    store,
+    name="1d.chunked.ragged.i2",
     dtype="int16",
-    chunk_shape=(2,),
+    chunks=(2,),
     shape=(5,),
-    codecs=[
-        zarrita.codecs.bytes_codec(),
-        zarrita.codecs.blosc_codec(typesize=4),
-    ],
+    compressors=[zarr.codecs.BloscCodec(typesize=4, shuffle="noshuffle")],
 )
 a[:] = [1, 2, 3, 4, 5]
 
 # 2d.contiguous.i2
-a = zarrita.Array.create(
-    store / "2d.contiguous.i2",
+a = zarr.create_array(
+    store,
+    name="2d.contiguous.i2",
     dtype="int16",
-    chunk_shape=(2, 2),
+    chunks=(2, 2),
     shape=(2, 2),
-    codecs=[
-        zarrita.codecs.bytes_codec(),
-        zarrita.codecs.blosc_codec(typesize=4),
-    ],
+    compressors=[zarr.codecs.BloscCodec(typesize=4, shuffle="noshuffle")],
 )
 a[:] = [[1, 2], [3, 4]]
 
 # 2d.chunked.i2
-a = zarrita.Array.create(
-    store / "2d.chunked.i2",
+a = zarr.create_array(
+    store,
+    name="2d.chunked.i2",
     dtype="int16",
-    chunk_shape=(1, 1),
+    chunks=(1, 1),
     shape=(2, 2),
-    codecs=[
-        zarrita.codecs.bytes_codec(),
-        zarrita.codecs.blosc_codec(typesize=4),
-    ],
+    compressors=[zarr.codecs.BloscCodec(typesize=4, shuffle="noshuffle")],
 )
 a[:] = [[1, 2], [3, 4]]
 
 # 2d.chunked.ragged.i2
-a = zarrita.Array.create(
-    store / "2d.chunked.ragged.i2",
+a = zarr.create_array(
+    store,
+    name="2d.chunked.ragged.i2",
     dtype="int16",
-    chunk_shape=(2, 2),
+    chunks=(2, 2),
     shape=(3, 3),
-    codecs=[
-        zarrita.codecs.bytes_codec(),
-        zarrita.codecs.blosc_codec(typesize=4),
-    ],
+    compressors=[zarr.codecs.BloscCodec(typesize=4, shuffle="noshuffle")],
 )
 a[:] = [[1, 2, 3], [4, 5, 6], [7, 8, 9]]
 
 # 3d.contiguous.i2
-a = zarrita.Array.create(
-    store / "3d.contiguous.i2",
+a = zarr.create_array(
+    store,
+    name="3d.contiguous.i2",
     dtype="int16",
-    chunk_shape=(3, 3, 3),
+    chunks=(3, 3, 3),
     shape=(3, 3, 3),
-    codecs=[
-        zarrita.codecs.bytes_codec(),
-        zarrita.codecs.blosc_codec(typesize=4),
-    ],
+    compressors=[zarr.codecs.BloscCodec(typesize=4, shuffle="noshuffle")],
 )
 a[:] = np.arange(27).reshape(3, 3, 3)
 
 # 3d.chunked.i2
-a = zarrita.Array.create(
-    store / "3d.chunked.i2",
+a = zarr.create_array(
+    store,
+    name="3d.chunked.i2",
     dtype="int16",
-    chunk_shape=(1, 1, 1),
+    chunks=(1, 1, 1),
     shape=(3, 3, 3),
-    codecs=[
-        zarrita.codecs.bytes_codec(),
-        zarrita.codecs.blosc_codec(typesize=4),
-    ],
+    compressors=[zarr.codecs.BloscCodec(typesize=4, shuffle="noshuffle")],
 )
 a[:] = np.arange(27).reshape(3, 3, 3)
 
 # 3d.chunked.mixed.i2.C
-a = zarrita.Array.create(
-    store / "3d.chunked.mixed.i2.C",
+a = zarr.create_array(
+    store,
+    name="3d.chunked.mixed.i2.C",
     dtype="int16",
-    chunk_shape=(3, 3, 1),
+    chunks=(3, 3, 1),
     shape=(3, 3, 3),
-    codecs=[
-        zarrita.codecs.bytes_codec(),
-        zarrita.codecs.blosc_codec(typesize=4),
-    ],
+    compressors=[zarr.codecs.BloscCodec(typesize=4, shuffle="noshuffle")],
 )
 a[:] = np.arange(27).reshape(3, 3, 3)
 
 # 3d.chunked.mixed.i2.F
-a = zarrita.Array.create(
-    store / "3d.chunked.mixed.i2.F",
+a = zarr.create_array(
+    store,
+    name="3d.chunked.mixed.i2.F",
     dtype="int16",
-    chunk_shape=(3, 3, 1),
+    chunks=(3, 3, 1),
     shape=(3, 3, 3),
-    codecs=[
-        zarrita.codecs.transpose_codec(order="F"),
-        zarrita.codecs.bytes_codec(),
-        zarrita.codecs.blosc_codec(typesize=4),
+    filters=[
+        zarr.codecs.TransposeCodec(order=[2, 1, 0])  # column major
     ],
+    compressors=[zarr.codecs.BloscCodec(typesize=4, shuffle="noshuffle")],
 )
 a[:] = np.arange(27).reshape(3, 3, 3)
 
@@ -270,279 +244,211 @@ a[:] = np.arange(27).reshape(3, 3, 3)
 
 # 1d.contiguous.compressed.sharded.i2
 data = np.array([1, 2, 3, 4], dtype="i2")
-path = "1d.contiguous.compressed.sharded.i2"
-a = zarrita.Array.create(
-    store / path,
+a = zarr.create_array(
+    store,
+    name="1d.contiguous.compressed.sharded.i2",
     shape=data.shape,
     dtype=data.dtype,
-    chunk_shape=(4,),
-    codecs=[
-        zarrita.codecs.sharding_codec(
-            chunk_shape=(4,),
-            codecs=[zarrita.codecs.bytes_codec(), zarrita.codecs.gzip_codec()],
-        )
-    ],
+    chunks=(4,),
+    shards=(4,),
+    compressors=[zarr.codecs.GzipCodec()],
 )
 a[:] = data
 
 # 1d.contiguous.compressed.sharded.i4
 data = np.array([1, 2, 3, 4], dtype="i4")
-path = "1d.contiguous.compressed.sharded.i4"
-a = zarrita.Array.create(
-    store / path,
+a = zarr.create_array(
+    store,
+    name="1d.contiguous.compressed.sharded.i4",
     shape=data.shape,
     dtype=data.dtype,
-    chunk_shape=(4,),
-    codecs=[
-        zarrita.codecs.sharding_codec(
-            chunk_shape=(4,),
-            codecs=[zarrita.codecs.bytes_codec(), zarrita.codecs.gzip_codec()],
-        )
-    ],
+    chunks=(4,),
+    shards=(4,),
+    compressors=[zarr.codecs.GzipCodec()],
 )
 a[:] = data
 
 # 1d.contiguous.compressed.sharded.u1
 data = np.array([255, 0, 255, 0], dtype="u1")
-path = "1d.contiguous.compressed.sharded.u1"
-a = zarrita.Array.create(
-    store / path,
+a = zarr.create_array(
+    store,
+    name="1d.contiguous.compressed.sharded.u1",
     shape=data.shape,
     dtype=data.dtype,
-    chunk_shape=(4,),
-    codecs=[
-        zarrita.codecs.sharding_codec(
-            chunk_shape=(4,),
-            codecs=[zarrita.codecs.bytes_codec(), zarrita.codecs.gzip_codec()],
-        )
-    ],
+    chunks=(4,),
+    shards=(4,),
+    compressors=[zarr.codecs.GzipCodec()],
 )
 a[:] = data
 
 
 # 1d.contiguous.compressed.sharded.<f4
 data = np.array([-1000.5, 0, 1000.5, 0], dtype="f4")
-path = "1d.contiguous.compressed.sharded.f4"
-a = zarrita.Array.create(
-    store / path,
+a = zarr.create_array(
+    store,
+    name="1d.contiguous.compressed.sharded.f4",
     shape=data.shape,
     dtype=data.dtype,
-    chunk_shape=(4,),
-    codecs=[
-        zarrita.codecs.sharding_codec(
-            chunk_shape=(4,),
-            codecs=[zarrita.codecs.bytes_codec(), zarrita.codecs.gzip_codec()],
-        )
-    ],
+    chunks=(4,),
+    shards=(4,),
+    compressors=[zarr.codecs.GzipCodec()],
 )
 a[:] = data
 
 # 1d.contiguous.compressed.sharded.f8
 data = np.array([1.5, 2.5, 3.5, 4.5], dtype="f8")
-path = "1d.contiguous.compressed.sharded.f8"
-a = zarrita.Array.create(
-    store / path,
+a = zarr.create_array(
+    store,
+    name="1d.contiguous.compressed.sharded.f8",
     shape=data.shape,
     dtype=data.dtype,
-    chunk_shape=(4,),
-    codecs=[
-        zarrita.codecs.sharding_codec(
-            chunk_shape=(4,),
-            codecs=[zarrita.codecs.bytes_codec(), zarrita.codecs.gzip_codec()],
-        )
-    ],
+    chunks=(4,),
+    shards=(4,),
+    compressors=[zarr.codecs.GzipCodec()],
 )
 a[:] = data
 
 
 # 1d.contiguous.compressed.sharded.b1
 data = np.array([True, False, True, False], dtype="b1")
-path = "1d.contiguous.compressed.sharded.b1"
-a = zarrita.Array.create(
-    store / path,
+a = zarr.create_array(
+    store,
+    name="1d.contiguous.compressed.sharded.b1",
     shape=data.shape,
     dtype="bool",
-    chunk_shape=(4,),
-    codecs=[
-        zarrita.codecs.sharding_codec(
-            chunk_shape=(4,),
-            codecs=[zarrita.codecs.bytes_codec(), zarrita.codecs.gzip_codec()],
-        )
-    ],
+    chunks=(4,),
+    shards=(4,),
+    compressors=[zarr.codecs.GzipCodec()],
 )
 a[:] = data
 
 # 1d.chunked.compressed.sharded.i2
 data = np.array([1, 2, 3, 4], dtype="i2")
-path = "1d.chunked.compressed.sharded.i2"
-a = zarrita.Array.create(
-    store / path,
+a = zarr.create_array(
+    store,
+    name="1d.chunked.compressed.sharded.i2",
     shape=data.shape,
     dtype=data.dtype,
-    chunk_shape=(2,),
-    codecs=[
-        zarrita.codecs.sharding_codec(
-            chunk_shape=(1,),
-            codecs=[zarrita.codecs.bytes_codec(), zarrita.codecs.gzip_codec()],
-        )
-    ],
+    chunks=(1,),
+    shards=(2,),
+    compressors=[zarr.codecs.GzipCodec()],
 )
 a[:] = data
 
 # 1d.chunked.filled.compressed.sharded.i2
 data = np.array([1, 2, 0, 0], dtype="i2")
-path = "1d.chunked.filled.compressed.sharded.i2"
-a = zarrita.Array.create(
-    store / path,
+a = zarr.create_array(
+    store,
+    name="1d.chunked.filled.compressed.sharded.i2",
     shape=data.shape,
     dtype=data.dtype,
-    chunk_shape=(2,),
-    codecs=[
-        zarrita.codecs.sharding_codec(
-            chunk_shape=(1,),
-            codecs=[zarrita.codecs.bytes_codec(), zarrita.codecs.gzip_codec()],
-        )
-    ],
+    chunks=(1,),
+    shards=(2,),
+    compressors=[zarr.codecs.GzipCodec()],
 )
 a[:] = data
 
-# # 1d.chunked.ragged.compressed.sharded.i2 (TypeError: 'int' object is not iterable)
-# data = array([1, 2, 3, 4, 5], dtype="i2")
-# path = "1d.chunked.ragged.compressed.sharded.i2"
-# a = zarrita.Array.create(
-#     store / path,
+# 1d.chunked.ragged.compressed.sharded.i2 (TypeError: 'int' object is not iterable)
+# data = np.array([1, 2, 3, 4, 5], dtype="i2")
+# a = zarr.create_array(
+#     store,
+#     name="1d.chunked.ragged.compressed.sharded.i2",
 #     shape=data.shape,
 #     dtype=data.dtype,
-#     chunk_shape=(2,),
-#     codecs=[
-#         zarrita.codecs.sharding_codec(
-#             chunk_shape=(1,),
-#             codecs=[
-#                 zarrita.codecs.blosc_codec(
-#                     typesize=data.dtype.itemsize, cname="gzip"
-#                 )
-#             ],
-#         )
-#     ],
+#     shards=(2,),
+#     chunks=(1,),
+#     compressors=[zarr.codecs.BloscCodec(typesize=data.dtype.itemsize, cname="gzip")],
 # )
 # a[:] = data
 
 # 2d.contiguous.compressed.sharded.i2
 data = np.arange(1, 5, dtype="i2").reshape(2, 2)
-path = "2d.contiguous.compressed.sharded.i2"
-a = zarrita.Array.create(
-    store / path,
+a = zarr.create_array(
+    store,
+    name="2d.contiguous.compressed.sharded.i2",
     shape=data.shape,
     dtype=data.dtype,
-    chunk_shape=(2, 2),
-    codecs=[
-        zarrita.codecs.sharding_codec(
-            chunk_shape=(2, 2),
-            codecs=[zarrita.codecs.bytes_codec(), zarrita.codecs.gzip_codec()],
-        )
-    ],
+    chunks=(2, 2),
+    shards=(2, 2),
+    compressors=[zarr.codecs.GzipCodec()],
 )
 a[:, :] = data
 
 # 2d.chunked.compressed.sharded.filled.i2
 data = np.arange(16, dtype="i2").reshape(4, 4)
-path = "2d.chunked.compressed.sharded.filled.i2"
-a = zarrita.Array.create(
-    store / path,
+a = zarr.create_array(
+    store,
+    name="2d.chunked.compressed.sharded.filled.i2",
     shape=data.shape,
     dtype=data.dtype,
-    chunk_shape=(2, 2),
-    codecs=[
-        zarrita.codecs.sharding_codec(
-            chunk_shape=(1, 1),
-            codecs=[zarrita.codecs.bytes_codec(), zarrita.codecs.gzip_codec()],
-        )
-    ],
+    chunks=(1, 1),
+    shards=(2, 2),
+    compressors=[zarr.codecs.GzipCodec()],
 )
 a[:, :] = data
 
 # 2d.chunked.compressed.sharded.i2
 data = np.arange(16, dtype="i2").reshape(4, 4) + 1
-path = "2d.chunked.compressed.sharded.i2"
-a = zarrita.Array.create(
-    store / path,
+a = zarr.create_array(
+    store,
+    name="2d.chunked.compressed.sharded.i2",
     shape=data.shape,
     dtype=data.dtype,
-    chunk_shape=(2, 2),
-    codecs=[
-        zarrita.codecs.sharding_codec(
-            chunk_shape=(1, 1),
-            codecs=[zarrita.codecs.bytes_codec(), zarrita.codecs.gzip_codec()],
-        )
-    ],
+    chunks=(1, 1),
+    shards=(2, 2),
+    compressors=[zarr.codecs.GzipCodec()],
 )
 a[:, :] = data
 
 # 2d.chunked.ragged.compressed.sharded.i2
 data = np.arange(1, 10, dtype="i2").reshape(3, 3)
-path = "2d.chunked.ragged.compressed.sharded.i2"
-a = zarrita.Array.create(
-    store / path,
+a = zarr.create_array(
+    store,
+    name="2d.chunked.ragged.compressed.sharded.i2",
     shape=data.shape,
     dtype=data.dtype,
-    chunk_shape=(2, 2),
-    codecs=[
-        zarrita.codecs.sharding_codec(
-            chunk_shape=(1, 1),
-            codecs=[zarrita.codecs.bytes_codec(), zarrita.codecs.gzip_codec()],
-        )
-    ],
+    chunks=(1, 1),
+    shards=(2, 2),
+    compressors=[zarr.codecs.GzipCodec()],
 )
 a[:, :] = data
 
 
 # 3d.contiguous.compressed.sharded.i2
 data = np.arange(27, dtype="i2").reshape(3, 3, 3)
-path = "3d.contiguous.compressed.sharded.i2"
-a = zarrita.Array.create(
-    store / path,
+a = zarr.create_array(
+    store,
+    name="3d.contiguous.compressed.sharded.i2",
     shape=data.shape,
     dtype=data.dtype,
-    chunk_shape=(3, 3, 3),
-    codecs=[
-        zarrita.codecs.sharding_codec(
-            chunk_shape=(3, 3, 3),
-            codecs=[zarrita.codecs.bytes_codec(), zarrita.codecs.gzip_codec()],
-        )
-    ],
+    chunks=(3, 3, 3),
+    shards=(3, 3, 3),
+    compressors=[zarr.codecs.GzipCodec()],
 )
 a[:, :, :] = data
 
 # 3d.chunked.compressed.sharded.i2
 data = np.arange(64, dtype="i2").reshape(4, 4, 4)
-path = "3d.chunked.compressed.sharded.i2"
-a = zarrita.Array.create(
-    store / path,
+a = zarr.create_array(
+    store,
+    name="3d.chunked.compressed.sharded.i2",
     shape=data.shape,
     dtype=data.dtype,
-    chunk_shape=(2, 2, 2),
-    codecs=[
-        zarrita.codecs.sharding_codec(
-            chunk_shape=(1, 1, 1),
-            codecs=[zarrita.codecs.bytes_codec(), zarrita.codecs.gzip_codec()],
-        )
-    ],
+    chunks=(1, 1, 1),
+    shards=(2, 2, 2),
+    compressors=[zarr.codecs.GzipCodec()],
 )
 a[:, :, :] = data
 
 # 3d.chunked.mixed.compressed.sharded.i2
 data = np.arange(27, dtype="i2").reshape(3, 3, 3)
-path = "3d.chunked.mixed.compressed.sharded.i2"
-a = zarrita.Array.create(
-    store / path,
+a = zarr.create_array(
+    store,
+    name="3d.chunked.mixed.compressed.sharded.i2",
     shape=data.shape,
     dtype=data.dtype,
-    chunk_shape=(3, 3, 3),
-    codecs=[
-        zarrita.codecs.sharding_codec(
-            chunk_shape=(3, 3, 1),
-            codecs=[zarrita.codecs.bytes_codec(), zarrita.codecs.gzip_codec()],
-        )
-    ],
+    chunks=(3, 3, 1),
+    shards=(3, 3, 3),
+    compressors=[zarr.codecs.GzipCodec()],
 )
 a[:, :, :] = data
