a97ee6c88869b2da61abe216ea08f3fca013d9d6
Trevor Manz <trevor.j.manz@gmail.com>
2025-02-26 12:09:39 -0500

    feat(zarrita): Re-export Slice type from @zarrita/indexing

--- packages/zarrita/index.ts
+++ packages/zarrita/index.ts
@@ -1,5 +1,5 @@
 export * from "@zarrita/core";
-export { get, set, slice } from "@zarrita/indexing";
+export { get, set, slice, type Slice, type Indices } from "@zarrita/indexing";
 export { default as FetchStore } from "@zarrita/storage/fetch";
 // re-export all the storage interface types
 export type * from "@zarrita/storage";
