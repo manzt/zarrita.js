843975cfc114c275ee29b6593edafb579ab41c16
Lazaro Alonso <lazarus.alon@gmail.com>
2025-10-24 11:45:32 +0200

    type guard

--- packages/zarrita/src/open.ts
+++ packages/zarrita/src/open.ts
@@ -118,7 +118,7 @@ async function _open_v3<Store extends Readable>(location: Location<Store>) {
 	}
 	let meta_doc: ArrayMetadata<DataType> | GroupMetadata =
 		json_decode_object(meta);
-	if (meta_doc.dimension_names){
+		if (meta_doc.node_type === "array" && meta_doc.dimension_names){
         meta_doc.attributes.dimension_names = meta_doc.dimension_names
     }
 	if (meta_doc.node_type === "array") {
